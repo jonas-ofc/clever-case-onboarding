d5efdec2c229c067bee5e56147f7cca1aa3f5c21
jonas <jonas.rossen@live.dk>
2025-01-20 15:00:05 +0100

    Statisk dashboard

---
+++ onboarding/src/views/DashboardView.vue
@@ -0,0 +1,16 @@
+<script>
+
+
+</script>
+
+<template>
+<img alt="Vue logo" src="../assets/clever-user-dashboard.png" class="static-background" />
+
+</template>
+
+<style scoped>
+.static-background {
+  width: 100vw;
+  
+}
+</style>
