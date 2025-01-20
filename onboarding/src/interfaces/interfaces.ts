01a2716c3a2d515dcddfaadb9d12e572e5001c86
jonas <jonas.rossen@live.dk>
2025-01-20 19:02:48 +0100

    Typescript interfaces i egen fil og folder

---
+++ onboarding/src/interfaces/interfaces.ts
@@ -0,0 +1,13 @@
+export interface Errors {
+    email: string;
+    password: string;
+}
+
+export interface User {
+    email: string;
+    password: string;
+}
+
+export interface ApiUser {
+    email: string;
+}
