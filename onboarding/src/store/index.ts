51b3d9b85f44aecc7d5c4c0aa02a7c999c824c54
jonas <jonas.rossen@live.dk>
2025-01-20 21:58:06 +0100

    Remove tracked files based on .gitignore

--- onboarding/src/store/index.ts
+++ onboarding/src/store/index.ts
@@ -1,27 +1,31 @@
 import { createStore } from 'vuex';
 import type { State } from '../interfaces/interfaces';
+import type { User } from '../interfaces/interfaces';
 
 export default createStore<State>({
-  state: {
-    isAuthenticated: false,
-  },
-  mutations: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-  actions: {
-    login({ commit }) {
-      commit('login');
-    },
-    logout({ commit }) {
-      commit('logout');
-    },
-  },
-  getters: {
-    isAuthenticated: (state) => state.isAuthenticated,
-  },
+    state: {
+        isAuthenticated: false,
+        user: null as User | null, // Tilføj brugerdata
+      },
+      mutations: {
+        setUser(state, user: User) {
+          state.user = user;
+        },
+        clearUser(state) {
+          state.user = null;
+        },
+      },
+      actions: {
+        async login({ commit }, user: User) {
+          commit('setUser', user);
+          commit('login');
+        },
+        logout({ commit }) {
+          commit('clearUser');
+          commit('logout');
+        },
+      },
+      getters: {
+        user: (state) => state.user,
+      },
 });
