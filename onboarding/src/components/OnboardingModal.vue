ed5198a32f741f96a86c069d9497f7cf0e1166db
jonas <jonas.rossen@live.dk>
2025-01-21 01:47:56 +0100

    Vis onboarding modal på dashboard

---
+++ onboarding/src/components/OnboardingModal.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "OnboardingModal",
+});
+
+</script>
+<template>
+  <div class="onboarding-modal">
+    <div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <h1>Welcome to the Clever User Dashboard!</h1>
+      <p>
+        We're excited to have you on board. To get started, please log in or create an account.
+      </p>
+    </div>        
+  </div>      
+</template>
--- onboarding/src/views/DashboardView.vue
+++ onboarding/src/views/DashboardView.vue
@@ -1,12 +1,26 @@
 <script lang="ts">
-export default {
-  name: "DashboardView",
-};
+import { defineComponent, ref } from "vue";
+import OnboardingModal from "../components/OnboardingModal.vue";
 
+export default defineComponent({
+  name: "DashboardView",
+  components: {
+    OnboardingModal,
+  },
+  setup() {
+    const showModal = ref(true);
+    return {
+      showModal,
+    };
+  },
+});
 </script>
 
 <template>
-<img alt="Vue logo" src="../assets/images/clever-user-dashboard.png" class="static-background" />
+
+<img alt="Dashboard" src="../assets/images/clever-user-dashboard.png" class="static-background" />
+
+<OnboardingModal v-if="showModal" />
 
 </template>
 
