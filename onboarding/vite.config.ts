a390189730e02867897ee5f1e731716a6ea01e74
jonas <jonas.rossen@live.dk>
2025-01-22 00:59:34 +0100

    test af onboarding store

--- onboarding/vite.config.ts
+++ onboarding/vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'; 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), svgLoader(),],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
