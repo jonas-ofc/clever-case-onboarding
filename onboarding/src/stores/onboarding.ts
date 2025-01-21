5c68efca802aca54693ee94382cf5521fed30f46
jonas <jonas.rossen@live.dk>
2025-01-21 19:54:06 +0100

    Tilføjelse af steps til onboarding flow

--- onboarding/src/stores/onboarding.ts
+++ onboarding/src/stores/onboarding.ts
@@ -3,10 +3,23 @@ import { defineStore } from "pinia";
 export const useOnboardingStore = defineStore("onboarding", {
   state: () => ({
     onboardingSeen: false,
+    currentStep: 0,
   }),
   actions: {
     setOnboardingSeen(seen: boolean) {
       this.onboardingSeen = seen;
+    },
+    nextStep() {
+      if (this.currentStep < 2) {
+        this.currentStep++;
+      } else {
+        this.setOnboardingSeen(true);
+      }
+    },
+    previousStep() {
+      if (this.currentStep > 0) {
+        this.currentStep--;
+      }
     },
   },
   persist: true
